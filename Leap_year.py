641fcc755df2c0a313e71f71e3387e83ed308c3b
Dipesh Bhatevera <35795916+DEEPESH98@users.noreply.github.com>
2019-07-26 10:44:24 +0530

    Create Leap_year.py

---
+++ Leap_year.py
@@ -0,0 +1,47 @@
+'''
+For an input year tell whether the year is leap or not. 
+
+Input:
+First line contains an integer, the number of test cases 'T' Each test case should contain a positive integer N(Year).
+
+
+Output:
+Print "Yes" if it is a leap year else "No". (Without the double quotes)
+
+
+Constraints:
+1<=T<=31
+1<=N<=9999
+
+
+Example:
+Input:
+1
+4
+
+Output:
+Yes
+'''
+
+For an input year tell whether the year is leap or not. 
+
+Input:
+First line contains an integer, the number of test cases 'T' Each test case should contain a positive integer N(Year).
+
+
+Output:
+Print "Yes" if it is a leap year else "No". (Without the double quotes)
+
+
+Constraints:
+1<=T<=31
+1<=N<=9999
+
+
+Example:
+Input:
+1
+4
+
+Output:
+Yes
